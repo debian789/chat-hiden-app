4e2830f8a9801559559ca390bfefd78938291d92
Miguel Suescun <debian789@gmail.com>
2018-06-11 10:40:32 -0500

    Ajustas plugin para subir archivos

--- src/chat/inputMessage/inputMessage.component.js
+++ src/chat/inputMessage/inputMessage.component.js
@@ -7,11 +7,26 @@
 import React, { Component } from 'react'
 import {
   View,
-  TextInput
+  TextInput,
+  TouchableHighlight
 } from 'react-native'
 
 import Icon from 'react-native-vector-icons/FontAwesome'
 import InputMessageStyle from './inputMessage.style'
+var ImagePicker = require('react-native-image-picker');
+var RNFS = require('react-native-fs');
+var uploadUrl = 'http://192.168.1.111:3000/upload';  // For testing purposes, go to http://requestb.in/ and create your own link
+
+const options = {
+  title: 'Select Avatar',
+//  customButtons: [
+//    {name: 'fb', title: 'Choose Photo from Facebook'},
+//  ],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+};
 
 export default class InputMessage extends Component {
   constructor (props) {
@@ -19,6 +34,7 @@ export default class InputMessage extends Component {
     this.state = {
       textInputValue: ''
     }
+    
   }
 
   _sendMensaje() {
@@ -28,10 +44,104 @@ export default class InputMessage extends Component {
     }
   }
 
+  _uploadFile() {
+    debugger
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+    
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+    
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+    // response.path 
+
+   
+    var files = [
+      {
+        name: 'file',
+        filename: response.fileName,
+        filepath: response.path ,
+        filetype: response.type
+      }
+    ];
+
+
+
+
+    var uploadBegin = (response) => {
+      var jobId = response.jobId;
+      console.log('UPLOAD HAS BEGUN! JobId: ' + jobId);
+    };
+    
+    var uploadProgress = (response) => {
+      var percentage = Math.floor((response.totalBytesSent/response.totalBytesExpectedToSend) * 100);
+      console.log('UPLOAD IS ' + percentage + '% DONE!');
+    };
+    
+    // upload files
+    RNFS.uploadFiles({
+      toUrl: uploadUrl,
+      files: files,
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+      },
+      fields: {
+        'hello': 'world',
+      },
+      begin: uploadBegin,
+      progress: uploadProgress
+    }).promise.then((response) => {
+        if (response.statusCode == 200) {
+          const urlImageUpload = JSON.parse(response.body)
+
+          urlImageUpload.file
+
+          debugger
+          //_sendMensaje() {
+          //  if (this.state.textInputValue){
+
+
+          console.log('FILES UPLOADED!'); // response.statusCode, response.headers, response.body
+        } else {
+          console.log('SERVER ERROR');
+        }
+      })
+      .catch((err) => {
+          debugger
+        if(err.description === "cancelled") {
+          // cancelled by user
+        }
+        console.log(err);
+      });
+
+
+      
+
+        //this.setState({
+        //  avatarSource: source
+        //});
+      }
+    });
+  }
+
   render () {
     return (
       <View style={InputMessageStyle.inputMessage}>
-        <Icon name="paperclip" style={InputMessageStyle.uploadFile} onPress={this.uploadFile}></Icon>
+        <TouchableHighlight onPress={this._uploadFile}>
+          <Icon name="paperclip" style={InputMessageStyle.uploadFile} ></Icon>
+        </TouchableHighlight>
         <TextInput
           underlineColorAndroid="transparent"
           style={InputMessageStyle.textMessage}
--- src/login/login.component.js
+++ src/login/login.component.js
@@ -1,23 +1,6 @@
 import React, {Component} from 'react'
 import {View, Text, TextInput, TouchableHighlight} from 'react-native';
 import LoginStyle from './login.style'
-var ImagePicker = require('react-native-image-picker');
-var RNFS = require('react-native-fs');
-var uploadUrl = 'http://192.168.1.111:3000/upload';  // For testing purposes, go to http://requestb.in/ and create your own link
-
-
-var options = {
-    title: 'Select Avatar',
-    customButtons: [
-      {name: 'fb', title: 'Choose Photo from Facebook'},
-    ],
-    storageOptions: {
-      skipBackup: true,
-      path: 'images'
-    }
-  };
-
-  
 
 export default class Login extends Component {
     constructor(props) {
@@ -41,88 +24,6 @@ export default class Login extends Component {
 
     _ingresar() {
         this.props.navigation.navigate('Chat', this.state)
-    }
-
-    _file() {
-        ImagePicker.showImagePicker(options, (response) => {
-            console.log('Response = ', response);
-          
-            if (response.didCancel) {
-              console.log('User cancelled image picker');
-            }
-            else if (response.error) {
-              console.log('ImagePicker Error: ', response.error);
-            }
-            else if (response.customButton) {
-              console.log('User tapped custom button: ', response.customButton);
-            }
-            else {
-              let source = { uri: response.uri };
-          
-              // You can also display the image using data:
-              // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-          // response.path 
-
-         
-          var files = [
-            {
-              name: 'file',
-              filename: response.fileName,
-              filepath: response.path ,
-              filetype: response.type
-            }
-          ];
-
-
-
-
-          var uploadBegin = (response) => {
-            var jobId = response.jobId;
-            console.log('UPLOAD HAS BEGUN! JobId: ' + jobId);
-          };
-          
-          var uploadProgress = (response) => {
-            var percentage = Math.floor((response.totalBytesSent/response.totalBytesExpectedToSend) * 100);
-            console.log('UPLOAD IS ' + percentage + '% DONE!');
-          };
-          
-          // upload files
-          RNFS.uploadFiles({
-            toUrl: uploadUrl,
-            files: files,
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-            },
-            fields: {
-              'hello': 'world',
-            },
-            begin: uploadBegin,
-            progress: uploadProgress
-          }).promise.then((response) => {
-              debugger
-              if (response.statusCode == 200) {
-                console.log('FILES UPLOADED!'); // response.statusCode, response.headers, response.body
-              } else {
-                console.log('SERVER ERROR');
-              }
-            })
-            .catch((err) => {
-                debugger
-              if(err.description === "cancelled") {
-                // cancelled by user
-              }
-              console.log(err);
-            });
-
-
-            
-
-              //this.setState({
-              //  avatarSource: source
-              //});
-            }
-          });
     }
 
     render() {
@@ -150,12 +51,7 @@ export default class Login extends Component {
                         <Text style={LoginStyle.button}>
                             Ingresar
                         </Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={this._file}>
-                        <Text style={LoginStyle.button}>
-                            adjuntr
-                        </Text>
-                    </TouchableHighlight>
+                    </TouchableHighlight>                    
                 </View>
             </View>
         )
